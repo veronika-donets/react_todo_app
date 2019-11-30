5a5ad25229afbc0981c11d9c35c47a57d0953490
Veronika D <donets.veronika@yahoo.com>
2019-11-30 20:20:16 +0200

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# react_todo_app
-
-- [DEMO LINK]
---
+++ precache-manifest.a6f5a6926efeaeb153f0e966b94871a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1306ffeab1703c61878412ec8bffaf68",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "14afd15477b0aba9ebfe",
+    "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
+  },
+  {
+    "revision": "2539ec89f1e54b86e139",
+    "url": "/react_todo-app/static/js/2.fb9c9243.chunk.js"
+  },
+  {
+    "revision": "14afd15477b0aba9ebfe",
+    "url": "/react_todo-app/static/js/main.d80afa53.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
